4f2167b37fd2efc8d9e0b4a4a14103a3c1e37b84
ayushsth123 <ayyush227@gmail.com>
2025-01-21 23:19:35 +0545

    Ingest Data with Django Rest framework

--- client/basic.py
+++ client/basic.py
@@ -5,11 +5,17 @@ endpoints="http://127.0.0.1:8000/api/"
 
 #aplication programming interface
 # get_response=requests.get(endpoints) #HTTP request
-get_response=requests.get(endpoints,params="123",json={"query":"hello ayush ji"})
+
+#FOR: GET
+# get_response=requests.get(endpoints,params="123",json={"query":"hello ayush ji"})
+
+#FOR: POST
+get_response=requests.post(endpoints,params="123",json={"id":4,"content":"hello ayush ji"})
+
 
 # print(get_response.text) #print raw text response
 # print(get_response.json()['message']) #print raw text response
 
 # print (get_response.status_code) #to print out the status code
-get_all_response=requests.get(endpoints)
+# get_all_response=requests.get(endpoints)
 print(get_response.json()) #print raw text response
